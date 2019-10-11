dd0a7cad2dde66a02fcfd0e4a9f45891b8d767e3
Ellen Waddle <elwa8931@colorado.edu>
2019-10-11 14:18:31 -0600

    forgot to add untitests on last commit

---
+++ test_data_import.py
@@ -0,0 +1,12 @@
+import data_import
+import unittest
+import sys
+import os
+
+class TestDataImport(unittest.TestCase):
+    def test_data_import_something(self):
+
+
+
+if __name__ == '__main__':
+    unittest.main()

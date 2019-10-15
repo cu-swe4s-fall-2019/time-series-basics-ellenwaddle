42ecc1f854b317406ea3b8c20b0ddf0ff6fd4a66
Ellen Waddle <elwa8931@colorado.edu>
2019-10-14 20:43:19 -0600

    adding more unit tests

--- test_data_import.py
+++ test_data_import.py
@@ -6,7 +6,9 @@ import os
 
 class TestDataImport(unittest.TestCase):
 
-    def test_data_print_array_(self):
+    def test_data_print_array_linear(self):
+        a = linear_search_value('this is not a time string')
+        assertEquals(a, 'invalid time')
 
 
 if __name__ == '__main__':
